446a385e8074b11bc7974787380c1369a0486cd2
Alex Marques <alexmarques2003@gmail.com>
2017-05-16 14:55:03 -0300

    Primeira commit do projeto onde foram criadas as classes

---
+++ crud-jpa-hibernate/src/com/algaworks/curso/main/AtualizandoPrimeiroObjeto.java
@@ -0,0 +1,25 @@
+package com.algaworks.curso.main;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import com.algaworks.curso.modelo.Cliente;
+
+public class AtualizandoPrimeiroObjeto {
+
+	public static void main(String[] args) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemploPU");
+		EntityManager em = emf.createEntityManager();
+		
+		Cliente cliente = em.find(Cliente.class, 2L);
+		em.getTransaction().begin();
+		cliente.setNome("Alex Marques Albuquerque");
+		cliente.setIdade(35);
+		em.getTransaction().commit();
+		
+		System.out.println("Cliente alterado com sucesso!");
+
+	}
+
+}
